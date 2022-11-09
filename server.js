dac30f02b4061df847e982f006ac83cd5f85b99c
bhanu6545 <10683588+bhanu6545@users.noreply.github.com>
2022-11-09 11:30:39 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+ 'use strict';
+
+ const express = require('express');
+
+ //Constants
+ const PORT = 8080;
+ //const HOST = '0.0.0.0';
+
+ //App
+ const app = express()
+ app.get('/',(req,res)=>{
+    res.send('Hello World node application');
+ });
+
+ app.listen(PORT, () => {
+   //console.log(`Running on http://${HOST}:${PORT}`);  
+   console.log(`Running on node application ${PORT}`)
+ });
+ 
+
+
